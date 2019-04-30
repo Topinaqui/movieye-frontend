8cd77cdea4e237663489768d38f67d4c7f668a08
Franklin Oliveira da Veiga <franklin.veiga@processmind.com.br>
2019-04-30 16:58:45 +0000

    Some adjusts for the request.

--- src/components/MoviesList/MoviesList.js
+++ src/components/MoviesList/MoviesList.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 
+import axios from "axios";
+
 import PropTypes from "prop-types";
 import { withStyles } from "@material-ui/core/styles";
 import List from "@material-ui/core/List";
@@ -8,6 +10,33 @@ import ListItemText from "@material-ui/core/ListItemText";
 import Typography from "@material-ui/core/Typography";
 
 class MoviesList extends Component {
+  apiFetcher() {
+    console.log("Vai chamar a API");
+
+    let form = new FormData();
+    form.set("query", "Rambo");
+
+    const http = axios.create({
+      baseURL: "http://localhost:8000/api",
+      headers: { "Access-Control-Allow-Origin": "*" }
+    });
+
+    http
+      .get("/search", {
+        query: "Rambo"
+      })
+      .then(({ data }) => {
+        console.log(data);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
+
+  componentDidMount() {
+    this.apiFetcher();
+  }
+
   render() {
     const { classes } = this.props;
     return (
@@ -22,12 +51,13 @@ class MoviesList extends Component {
                   className={classes.inline}
                   color="textPrimary"
                 >
-                  Sinopse
-                </Typography>
-                {" Movie Sinopse... ... ..."}
+                  {" "}
+                  Sinopse{" "}
+                </Typography>{" "}
+                {" Movie Sinopse... ... ..."}{" "}
               </React.Fragment>
             }
-          />
+          />{" "}
         </ListItem>
       </List>
     );
